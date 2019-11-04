da41e458d37669e82293e6a88f4b6b51d7f7f069
yancy <986244073@qq.com>
2019-11-04 18:07:21 +0800

    主页
    主页

--- App.vue
+++ App.vue
@@ -14,4 +14,6 @@
 
 <style>
 	/*每个页面公共css */
+	@import './common/uni.css';
+	@import './common/iconfont.css';
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,65 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="view__body">
+		
+		<view class="view__item" v-for="(item, index) in dataList" :key="index">
+			<view class="view__item-title">{{ item.title }}</view>
+			<view class="view__item-author">{{ item.author }}</view>
+			<view class="view__item-icon"> 
+			{{ item.collect }}
+			 <text class="iconfont icon icon-collected"></text>
+			 {{ item.like }}
+			 <text class="iconfont icon icon-like"></text>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {
+			title: 'Hello',
+			dataList: [
+				{ title: 'i let Blank Notebook Censor Me', collect: 155, like: 235, author: 'alen walker' },
+				{ title: 'i let Blank Notebook Censor Me', collect: 155, like: 235, author: 'alen walker' },
+				{ title: 'i let Blank Notebook Censor Me', collect: 155, like: 235, author: 'alen walker' },
+				{ title: 'i let Blank Notebook Censor Me', collect: 155, like: 235, author: 'alen walker' },
+				{ title: 'i let Blank Notebook Censor Me', collect: 155, like: 235, author: 'alen walker' },
+				{ title: 'i let Blank Notebook Censor Me', collect: 155, like: 235, author: 'alen walker' },
+			]
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+	@font-face {
+		font-family:'iconfont';
+		src: url('https://at.alicdn.com/t/font_1491397_y8j40in8dg.css');
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.view__body {
+	background-color: rgb(45, 78, 138);
+}
+.view__item {
+	margin: 10rpx;
+	background: #4CD964;
+	border-radius: 5px;
+	height: 300upx;
+	width:98%;
+}
+.view__item-title{
+	padding-top: 40upx;
+	text-align: center;
+}
+.view__item-author{
+	text-align: center;
+}
+.view__item-icon{
+	text-align: center;
+}
+.view__item-like-img{
+	font-size: 1rpx;
+}
 </style>
